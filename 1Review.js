e11954d9d79b2166e57bf6f4aaa631c1e052d1fe
Sameer <bhandarisameer173@gmail.com>
2025-02-08 19:03:30 +0545

    Review

---
+++ 1Review.js
@@ -0,0 +1,31 @@
+// Review For loop 
+ 
+for(i =0 ; i <= 5 ; i ++ ){
+    console.log("Sameer")
+ }
+  console.log("Ended")
+
+
+  // For printing n sum of numbers 
+  {
+    let num = 8;
+    let sum = 0 ;
+    let i ;
+    for(i = 1 ; i <= num ; i++){
+          sum = sum + i ;
+          console.log("The sum is " , sum)
+    }
+    console.log("Ended ")
+  }
+
+
+  // While loop 
+{
+    let num = 10;
+    let i = 0 ;
+    while(i <= num ){
+        console.log("sameer");
+        i++;
+    }
+    console.log("Ended");
+}
