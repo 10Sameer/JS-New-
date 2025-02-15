5912ba19dfc5961e6442257c6b9e1852f9c6fbfd
Sameer <bhandarisameer173@gmail.com>
2025-02-15 22:11:10 +0545

    Concat

--- arrayMethod.js
+++ arrayMethod.js
@@ -22,3 +22,38 @@ num.push(10);
     console.log(name) ; // nad is deleted
     console.log('Deleted name =', deletedName)
 }
+
+// toString method used to print array basic from
+// seperated by commas 
+{
+    let name =['sam','eer','bha','nad','ri'];
+    let marks=[100,95,89,96,84,78];
+    console.log(name);
+    console.log(name.toString());
+    console.log(marks.toString());
+}
+
+// concat to join multiple arrays 
+// concat makes new array by combining these arrays
+{
+    let car = ["Volvo", "BMW", "Ford", "Mazda"];
+    let superCars = ['Supra', 'RollsRoyce','Lambo','davidPutra'];
+    let cars = car.concat(superCars);
+    console.log(car);
+    console.log(superCars);
+    console.log(cars);
+    console.log(cars.toString());
+}
+
+// can pass mutiple arrays 
+{
+    let manU = ['Degea','licha','Cr7','deligt','Harry'];
+    let barca = ['Messi','stegan','busquet','puyol','xavi'];
+    let sagarMatha = ['Sameer','Kc','Rawal','ojha','Tamang',
+        'chaudary','Ambani','Praj'];
+        console.log(manU);
+        console.log(barca);
+        console.log(sagarMatha);
+    let bestTeam = sagarMatha.concat(barca,manU);
+    console.log(bestTeam.toString())
+}
