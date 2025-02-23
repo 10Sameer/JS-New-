5d34ee905208643e725128943489e394c8c7a75c
Sameer <bhandarisameer173@gmail.com>
2025-02-23 20:57:19 +0545

    functionsyntax

---
+++ function1.js
@@ -0,0 +1,22 @@
+// creating a simple function
+ /* Basic form of Function 
+  function anyFunctionName(){
+        block of code
+  }
+
+   recalling the function = anyFunctionName();
+} */
+
+function functionName(){
+    console.log("Sameer Bhandari");
+    console.log("Hello Im learing javascript");
+    let a = [100,200,300,400,500,600,700,800];
+    console.log(a.toString());
+    let car = ["Toyota", "Honda", "Ford", "BMW", "Mercedes", "Audi", "Tesla"];
+    let newcar = ["Ferrari", "Lamborghini", "Bugatti", "Koenigsegg", "Pagani"];
+    let Cars = car.concat(newcar);
+    console.log(Cars.toLocaleString());
+}
+ functionName();
+ functionName();
+ functionName();
